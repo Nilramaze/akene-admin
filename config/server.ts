ebda3685e3c131ef47688ada2a4fc50a9f9a0cec
Nilramaze <96390928+Nilramaze@users.noreply.github.com>
2025-04-02 21:08:12 +0200

    APP Keys

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,12 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', [
+      '8BK1+FNpbLPZXWHFBfmehw==',
+      'SOIg+0EKPoAXPRTO6ZWB3g==',
+      'nJpbK6aUB8FlJo0+3xYwlw==',
+      'AoB5JCUEl1RRkbcIPfXjxw==',
+    ]),
   },
 });
