d6c8c7a25cde634995f7e4eb523b67cf09ffada3
Nilramaze <96390928+Nilramaze@users.noreply.github.com>
2025-04-29 14:57:52 +0200

    cors aktivate

--- dist/config/middlewares.js
+++ dist/config/middlewares.js
@@ -7,7 +7,7 @@ exports.default = [
     {
         name: 'strapi::cors',
         config: {
-            origin: ['http://localhost:1337', 'http://akene.test-gait.de', 'https://akene.test-gait.de', 'http://localhost:5173'], // Erlaube deine Domains
+            origin: ['http://localhost:1337', 'http://akene.test-gait.de', 'https://akene.test-gait.de', 'http://localhost:5173', 'https://localhost:5173'], // Erlaube deine Domains
             methods: ['*'],
             headers: ['Content-Type', 'Authorization'],
         },
